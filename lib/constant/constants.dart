597cd244bde68011b584d4fff7910b93a5741c0c
shaharyar <shaharyar4t4@gmail.com>
2025-12-23 16:10:10 +0500

    inital setup of font style and it's main file

---
+++ lib/constant/constants.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  
+  static const kPrimary = Color(0xFFfcba03);
+
+  static const MaterialColor kPrimarySwatch = MaterialColor(
+    0xFFfcba03,
+    <int, Color>{
+      50: Color(0xFFFFF8E1),
+      100: Color(0xFFFFECB3),
+      200: Color(0xFFFFE082),
+      300: Color(0xFFFFD54F),
+      400: Color(0xFFFFCA28),
+      500: Color(0xFFFFC107),
+      600: Color(0xFFFFB300),
+      700: Color(0xFFFFA000),
+      800: Color(0xFFFF8F00),
+      900: Color(0xFFFF6F00),
+    },
+  );
+}
